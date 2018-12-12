a8ae41adaf629ff906f10d563bef275d0848663c
zhaoyuanfang <mojie_fang@foxmail.com>
2018-12-12 08:14:28 +0800

    confused by git

---
+++ src/gui/panel/WorkingPanel.java
@@ -0,0 +1,9 @@
+package gui.panel;
+
+import javax.swing.JPanel;
+
+public abstract class WorkingPanel extends JPanel {
+	public abstract void updataData();
+
+	public abstract void addListener();
+}
